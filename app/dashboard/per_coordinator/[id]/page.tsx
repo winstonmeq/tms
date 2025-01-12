c088991639eb2551fd1333c404150bde9c5e0f61
winstonmeq <winstonmequila@gmail.com>
2025-01-12 14:40:14 +0800

    update prisma to generate

--- app/dashboard/per_coordinator/[id]/page.tsx
+++ app/dashboard/per_coordinator/[id]/page.tsx
@@ -1,11 +1,35 @@
 
 
-import React from 'react'
 
-const page = () => {
+
+import React from 'react'
+import { DataTable } from './datatables'
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+
+
+
+
+const PerCoordinatorPage = async () => {
+
+  const session = await auth.api.getSession({
+    headers: await headers()
+  });
+
+  if (!session) {
+    return redirect('/sign-in/')
+  }
+
+  // const user = session?.user;
+
+
   return (
-    <div>page</div>
+    <div className="container mx-auto py-10">
+      Per Coordinator
+      <DataTable />
+    </div>
   )
 }
 
-export default page
+export default PerCoordinatorPage

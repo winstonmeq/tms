ef44f3ded53f6196cb0efd2b5dd170c9ac846889
winstonmeq <winstonmequila@gmail.com>
2025-01-12 15:01:09 +0800

    add web analytics from vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react"
 
 
 const geistSans = Geist({
@@ -29,13 +30,10 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         
-          {/* <Navbar />
-          <MunListPage /> */}
-            {children}
-        
+            <Analytics />
+            {children}       
 
-          {/* <Toaster />
-         */}
+        
         
        
       
